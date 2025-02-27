2b5a12df2df153e53be450ddb4c249295d855b17
515nika <pirogovaveronika06@mail.ru>
2025-02-27 11:51:37 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,9 +3,7 @@ s = itertools.product("ГЕПАРД", repeat=5)
 a = []
 l = []
 cnt = 0
-for i in s:
-    a.append(list(i))
-for e in a:
+for e in s:
     if e.count("Г") == 1 and e[0] != "А" and e[-1] != "Е":
         l.append(e)
         cnt += 1
